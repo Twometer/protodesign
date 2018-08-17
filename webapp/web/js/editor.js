2cb00fcf2efe59f0ace245817acd280e2b5c180c
Twometer <twometer@outlook.de>
2018-08-17 16:20:08 +0200

    Fixed various bugs with the new editor

--- webapp/web/js/editor.js
+++ webapp/web/js/editor.js
@@ -1,23 +1,32 @@
 var $content = $("#inputContent");
 
 var brackets = {
-    '{' : '}',
-    '(' : ')',
-    '[' : ']',
-    '<' : '>'
+    '{': '}',
+    '(': ')',
+    '[': ']',
+    '<': '>'
 };
 
 function findIndentation(currentIdx) {
+    currentIdx -= 1;
     var val = $content.val();
-    var lines = val.split('\n');
-    var theLine = lines[lines.length - 1];
-    var indent = 0;
-    for (var i = 0; i < theLine.length; i++) {
-        if (theLine[i] === ' ')
-            indent++;
-        else break;
+    var lineStart = 0;
+    for (var i = currentIdx; i > 0; i--) {
+        var charCode = val.charCodeAt(i);
+        if (charCode === 10 || charCode === 13) {
+            lineStart = i;
+            break;
+        }
     }
-    return indent;
+    var indent = 0;
+    var currentLine = val.substr(lineStart, currentIdx - lineStart);
+    for (var i = 0; i < currentLine.length; i++) {
+        var charCode = currentLine.charCodeAt(i);
+        if (charCode === 32)
+            indent++;
+        else if (charCode !== 10 && charCode !== 13) break;
+    }
+    return indent + 1 === currentLine.length ? 0 : indent;
 }
 
 function insertText(text, insertion, idx) {
@@ -51,5 +60,28 @@ $content.bind('keydown', function (e) {
         $this.prop('selectionEnd', function (i, t) {
             return t - 1;
         });
+    } else if (Object.values(brackets).indexOf(e.key) >= 0) {
+        if ($(this).val()[$(this).prop('selectionStart')] === e.key) {
+            e.preventDefault();
+            $this.prop('selectionStart', function (i, t) {
+                return t + 1;
+            });
+            $this.prop('selectionEnd', function (i, t) {
+                return t + 1;
+            });
+        }
+    } else if (e.keyCode === 9) {
+        e.preventDefault();
+        var idx = $(this).prop('selectionStart');
+        var indent = findIndentation(idx);
+        var indentString = "";
+        for (var i = indent + 1; i < 4; i++) {
+            indentString += " ";
+        }
+        $content.val(function (i, text) {
+            return insertText(text, indentString, idx);
+        });
+        $(this).prop('selectionStart', idx + indentString.length);
+        $(this).prop('selectionEnd', idx + indentString.length);
     }
 });
