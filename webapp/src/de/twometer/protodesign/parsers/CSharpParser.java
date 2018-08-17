7d1d6146157cb9803e8b901b485dcac0741815e8
Twometer <twometer@outlook.de>
2018-08-17 17:00:20 +0200

    Improved CSharp syntax highlighting

--- webapp/src/de/twometer/protodesign/parsers/CSharpParser.java
+++ webapp/src/de/twometer/protodesign/parsers/CSharpParser.java
@@ -10,13 +10,15 @@ import static de.twometer.protodesign.util.ParserUtils.*;
 public class CSharpParser implements IParser {
 
     private static final List<String> keywords = Arrays.asList("abstract", "add", "as", "ascending", "async", "await", "base", "bool", "break", "by", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "descending", "do", "double", "dynamic", "else", "enum", "event", "equals", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "from", "get", "global", "goto", "group", "if", "implicit", "in", "int", "interface", "internal", "into", "is", "join", "let", "lock", "long", "namespace", "new", "null", "object", "on", "operator", "orderby", "out", "override", "params", "partial", "private", "protected", "public", "readonly", "ref", "remove", "return", "sbyte", "sealed", "select", "set", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "value", "var", "virtual", "void", "volatile", "where", "while", "yield", "#region", "#endregion");
-
+    private static final List<String> typeKeywords = Arrays.asList("class", "interface", "enum");
 
     private class State {
         int closeTag = -1;
         boolean inSingleComment = false;
         boolean inBlockComment = false;
         boolean inString = false;
+        boolean schouldBeType = false;
+        boolean waitingEndOfType = false;
         int inStringTF = 0;
 
         boolean inComment() {
@@ -28,10 +30,13 @@ public class CSharpParser implements IParser {
     public String process(String html) {
         State state = new State();
         return CodeScanner.scan(html, "csharp", (data, i, output) -> {
-            String x = anyStartsAt(data, keywords, i);
-            if (x != null && state.closeTag == -1 && (isValidKeywordStarter(data, i)) && isValidKeywordEnd(data, i, x) && !state.inComment()) {
+            String keyword = anyStartsAt(data, keywords, i);
+            String typeKeyword = anyStartsAt(data, typeKeywords, i);
+            if (keyword != null && state.closeTag == -1 && (isValidKeywordStarter(data, i)) && isValidKeywordEnd(data, i, keyword) && !state.inComment() && !state.inString) {
+                if(typeKeyword != null && !state.waitingEndOfType)
+                    state.schouldBeType = true;
                 output.append(buildHtmlTag("csharp-keyword", true));
-                state.closeTag = i + x.length() - 1;
+                state.closeTag = i + keyword.length() - 1;
             }
             if (startsAt(data, "//", i) && !state.inSingleComment) {
                 state.inSingleComment = true;
@@ -63,6 +68,15 @@ public class CSharpParser implements IParser {
             if (state.closeTag == i && state.closeTag > 0) {
                 output.append(buildHtmlTag("csharp-keyword", false));
                 state.closeTag = -1;
+            }
+            if(!Character.isLetterOrDigit(data[i]) && state.waitingEndOfType){
+                state.waitingEndOfType = false;
+                output.append(buildHtmlTag("csharp-datatype", false));
+            }
+            if(data[i] == ' ' && state.schouldBeType){
+                output.append(buildHtmlTag("csharp-datatype", true));
+                state.schouldBeType = false;
+                state.waitingEndOfType = true;
             }
         });
     }
